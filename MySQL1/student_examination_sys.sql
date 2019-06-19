892eca947bb9bbda186019c23b06c1698406f4dc
VeuraGJ <yuanxiao_ru@163.com>
2019-06-19 20:37:00 +0800

    update student_examination_sys.sql

--- MySQL1/student_examination_sys.sql
+++ MySQL1/student_examination_sys.sql
@@ -1,76 +1,41 @@
-/*
-Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50717
-Source Host           : localhost:3306
-Source Database       : student_examination_sys
+CREATE TABLE score (
+  id varchar(10) NOT NULL,
+  student_id varchar(10) NOT NULL,
+  subject_id varchar(10) NOT NULL,
+  score double NOT NULL,
+  PRIMARY KEY (id),
+  KEY score_std (student_id),
+  KEY score_sub (subject_id),
+  CONSTRAINT score_std FOREIGN KEY (student_id) REFERENCES `student` (`id`),
+  CONSTRAINT score_sub FOREIGN KEY (subject_id) REFERENCES `subject` (`id`)
+) ;
 
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
+INSERT INTO score VALUES ('1', '001', '1001', '80');
+INSERT INTO score VALUES ('2', '002', '1002', '60');
+INSERT INTO score VALUES ('3', '001', '1001', '70');
+INSERT INTO score VALUES ('4', '002', '1002', '60.5');
 
-Date: 2019-06-19 20:21:51
-*/
 
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for `score`
--- ----------------------------
-DROP TABLE IF EXISTS `score`;
-CREATE TABLE `score` (
-  `id` varchar(10) NOT NULL,
-  `student_id` varchar(10) NOT NULL,
-  `subject_id` varchar(10) NOT NULL,
-  `score` double NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `score_std` (`student_id`),
-  KEY `score_sub` (`subject_id`),
-  CONSTRAINT `score_std` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`),
-  CONSTRAINT `score_sub` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of score
--- ----------------------------
-INSERT INTO `score` VALUES ('1', '001', '1001', '80');
-INSERT INTO `score` VALUES ('2', '002', '1002', '60');
-INSERT INTO `score` VALUES ('3', '001', '1001', '70');
-INSERT INTO `score` VALUES ('4', '002', '1002', '60.5');
-
--- ----------------------------
--- Table structure for `student`
--- ----------------------------
-DROP TABLE IF EXISTS `student`;
-CREATE TABLE `student` (
-  `id` varchar(10) NOT NULL,
-  `name` varchar(6) NOT NULL,
-  `age` int(11) NOT NULL,
-  `sex` enum('女','男') NOT NULL DEFAULT '男',
+CREATE TABLE student (
+  id varchar(10) NOT NULL,
+  name varchar(6) NOT NULL,
+  age int(11) NOT NULL,
+  sex enum('女','男') NOT NULL DEFAULT '男',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
--- ----------------------------
--- Records of student
--- ----------------------------
-INSERT INTO `student` VALUES ('001', '张三', '18', '男');
-INSERT INTO `student` VALUES ('002', '李四', '20', '女');
+INSERT INTO student VALUES ('001', '张三', '18', '男');
+INSERT INTO student VALUES ('002', '李四', '20', '女');
 
--- ----------------------------
--- Table structure for `subject`
--- ----------------------------
-DROP TABLE IF EXISTS `subject`;
-CREATE TABLE `subject` (
-  `id` varchar(10) NOT NULL,
-  `subject` varchar(8) NOT NULL,
-  `teacher` varchar(8) NOT NULL,
-  `description` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records of subject
--- ----------------------------
-INSERT INTO `subject` VALUES ('1001', '语文', '王老师', '本次考试比较简单');
-INSERT INTO `subject` VALUES ('1002', '数学', '刘老师', '本次考试比较难');
+CREATE TABLE subject (
+  id varchar(10) NOT NULL,
+  subject varchar(8) NOT NULL,
+  teacher varchar(8) NOT NULL,
+  description varchar(30) DEFAULT NULL,
+  PRIMARY KEY (id)
+) 
+
+INSERT INTO subject VALUES ('1001', '语文', '王老师', '本次考试比较简单');
+INSERT INTO subject VALUES ('1002', '数学', '刘老师', '本次考试比较难');
